9dcc69081d55121b877b8bcc015c47d391d126a8
coomeg <ao.kcghiro@gmail.com>
2020-02-29 20:19:44 +0900

    新規作成

---
+++ kanban-server/src/main/java/com/jp/kanbannapp/KanbannappApplication.java
@@ -0,0 +1,13 @@
+package com.jp.kanbannapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbannappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbannappApplication.class, args);
+	}
+
+}
---
+++ kanban-server/src/test/java/com/jp/kanbannapp/KanbannappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.kanbannapp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbannappApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//		System.out.println("test");
+//	}
+
+}
