3501a9dc44b376237b5ab24f3f1958b8249ac51d
coomeg <ao.kcghiro@gmail.com>
2020-03-06 22:46:26 +0900

    binフォルダを管理から外す

---
+++ kanban-server/src/main/java/com/jp/kanbannapp/api/TestController.java
@@ -0,0 +1,18 @@
+package com.jp.kanbannapp.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/v1/users")
+public class TestController {
+
+	@GetMapping("")
+    public String findById() {
+        return "test:";
+    }
+}
