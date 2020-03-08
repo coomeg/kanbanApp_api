5b6e6f61a4c72e3a5b262fe49ef2691560a07f8b
coomeg <ao.kcghiro@gmail.com>
2020-03-08 20:29:41 +0900

    sessionの保存先をredisにする

--- kanban-server/src/main/java/com/jp/kanbannapp/KanbanSecurityConfig.java
+++ kanban-server/src/main/java/com/jp/kanbannapp/KanbanSecurityConfig.java
@@ -2,15 +2,38 @@ package com.jp.kanbannapp;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class KanbanSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    	http.authorizeRequests()
-    		.mvcMatchers("/**").permitAll();// すべて許可
+    	// ログイン処理の認証ルールを設定
+        http.authorizeRequests()
+                .antMatchers("/",
+                		"/login").permitAll() // 認証なしでアクセス可能なパス
+                .anyRequest().authenticated() // それ以外は認証が必要
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/public/login") // ログインフォームのアクションに指定したURL[action="@{/login}"]を設定
+                .usernameParameter("userId") // ログインフォームのユーザー欄のname属性を設定
+                .passwordParameter("password") // ログインフォームのパスワード欄のname属性を設定
+                .successForwardUrl("/") // ログイン成功時に遷移するURL
+                .permitAll()
+                .and()
+            .logout()
+                .logoutUrl("/logout")
+                .permitAll()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/favicon.ico", "/css/**", "/js/**", "/images/**", "/fonts/**");
     }
 }
